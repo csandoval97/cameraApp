ec4531b7ac4a583da252ec49751cc62e7ab925d2
Charly Sandoval <csandoval.xbox@gmail.com>
2020-05-22 21:12:38 +0100

    tryied image update using div

--- public/switch.js
+++ public/switch.js
@@ -1,12 +1,3 @@
-var btnState = 0;
-
-function turnOn(){
-    btnState = 1;
-}
-function turnOff(){
-    btnState = 0;
-}
-
 function On(){
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function(){
@@ -18,8 +9,21 @@ function On(){
     xhttp.send()
 }
 
-function repeat(){
-    setInterval( function(){if(btnState){
-        On();
-    }},300)
+function Off(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        if(this.readyState==4 && this.status==200){
+            //cFunction(this);
+        }
+    }
+    xhttp.open('GET','/off',true);
+    xhttp.send()
 }
+
+setInterval(()=>{
+    // document.getElementById('camImage').src = `http://10.0.0.9/image/index?time=${Date.now()}`
+    var imgURL = `http://10.0.0.9/image/index?time=${Date.now()}`
+    console.log(imgURL)
+    document.getElementById('image').style.backgroundImage = `url(${imgURL})`
+    // console.log(Date.now())
+},300)
